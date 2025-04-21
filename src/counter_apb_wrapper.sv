491c55601e316ce2d03cf3972118565a700c3647
Daniel Iglesias <danieliglesias.chile@gmail.com>
2025-04-21 00:01:11 -0400

    Update APB

--- src/counter_apb_wrapper.sv
+++ src/counter_apb_wrapper.sv
@@ -1,6 +1,6 @@
 module counter_apb_wrapper #(
-    parameter WIDTH = 32,
-    parameter ADDRESS = 0x01 // Initial address
+    parameter WIDTH = 32, // Count size
+    parameter ADDRESS = 1 // Initial address
 ) (
     input pclk,
     input presetn, 
@@ -14,27 +14,31 @@ module counter_apb_wrapper #(
     output logic pslverr
 );
 
+    // Registers
     logic [31:0] r_en; // RW
     logic [31:0] r_rst_n; // RW
     logic [31:0] r_dir; // RW
-    logic [31:0] r_count; // Only read
+    logic [31:0] r_count; // R
 
+    // Address
     localparam ADDR_EN = ADDRESS;
     localparam ADDR_RST = ADDRESS + 4;
     localparam ADDR_DIR = ADDRESS + 8;
     localparam ADDR_COUNT = ADDRESS + 12;
+
     localparam INVALID = 32'hDEADBEEF;
 
-    assign pready = 1'b1;
-    assign pslverr = 1'b0;
+    assign pready = 1'b1; // Count is always ready
+    assign pslverr = 1'b0; // Not error are consider
 
+    // Update or reset register
     always_ff @(posedge pclk) begin
         if (!presetn) begin
             r_dir <= 0;
             r_en <= 0;
             r_rst_n <= 0;
         end else begin
-            if (penable && psel) begin
+            if (penable && psel) begin // Only if en and sel are valid, you can write
                 if (pwrite) begin
                     case (paddr)
                         ADDR_EN:    r_en <= pwdata;
@@ -46,7 +50,9 @@ module counter_apb_wrapper #(
         end
     end
 
+    // Read register
     always_comb begin
+        prdata = INVALID; // Avoid that prdata remains without value
         if (!pwrite && penable && psel) begin
             case (paddr)
                 ADDR_EN:    prdata = r_en;
@@ -60,11 +66,11 @@ module counter_apb_wrapper #(
     
     counter #(
         .WIDTH(WIDTH)
-    ) (
+    ) COUNTER (
         .clk(pclk),
-        .en(r_en),
-        .rst_n(r_rst_n),
-        .dir(r_dir),
+        .en(r_en[0]),
+        .rst_n(r_rst_n[0]),
+        .dir(r_dir[0]),
         .count(r_count)
     );
 endmodule
